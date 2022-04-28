fec24a678dc6957a771129d5bde1b04af2c5a180
nataliaaraujo0 <devjrteste@gmail.com>
2022-04-27 22:01:28 -0300

    primeiro teste de merge dev par amain

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>teste merge dev</p>
+        <p>DEV :teste de merge</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
